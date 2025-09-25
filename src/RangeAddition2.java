9142f294ebb157241e0137215702eca1f74fa1e8
Sane Generous <karim2k11@gmail.com>
2025-09-25 15:20:40 +0100

    Range Addition 2.

---
+++ src/RangeAddition2.java
@@ -0,0 +1,18 @@
+/*
+* You are given an m x n matrix M initialized with all 0's and an array of operations ops,
+* where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
++
+Count and return the number of maximum integers in the matrix after performing all the operations.
+* */
+
+
+public class RangeAddition2 {
+    public int maxCount(int m, int n, int[][] ops) {
+        int minRow = m, minCol = n;
+        for(int[] op : ops){
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+        return minRow*minCol;
+    }
+}
